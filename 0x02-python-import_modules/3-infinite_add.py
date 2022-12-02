5d01456e1633dc133ab1f142f8a5717b26e87724
David Onuta <ogagaonuta@gmail.com>
2022-12-02 05:35:38 +0000

    Print the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,11 +3,8 @@
 if __name__ == "__main__":
     import sys
     sum = 0
-    if (len(sys.argv) == 1):
-        print("{}".format(sum))
-    else:
-        i = 1
-        while (i < len(sys.argv)):
-            sum += int(sys.argv[i])
-            i += 1
+    i = 1
+    while (i < len(sys.argv)):
+        sum += int(sys.argv[i])
+        i += 1
     print("{}".format(sum))
