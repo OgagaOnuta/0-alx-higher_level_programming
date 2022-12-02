05eccaf52230b58668384d6d678c7ea95292e86a
David Onuta <ogagaonuta@gmail.com>
2022-12-02 06:28:07 +0000

    Print all the names defined by a compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@
 if __name__ == "__main__":
     from hidden_4 import *
     i = 0
-    while (i < len(dir())):
-        print("{}".format(dir()[i]))
+    while (i < len(dir(hidden_4))):
+        print("{}".format(dir(hidden_4)[i]))
         i += 1
