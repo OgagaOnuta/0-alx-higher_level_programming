c89ed109519df6bdf8852dfaf77a8b7148f0b548
David Onuta <ogagaonuta@gmail.com>
2022-12-21 02:28:20 +0000

    Write a class that defines a square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+Creates a class Square that defines a square
+'''
+
+
+class Square:
+    '''
+    Defines a square
+    '''
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if (type(self.__size) is not int):
+            raise TypeError("size must be an integer")
+        if (self.__size < 0):
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return (self.__size * self.__size)
