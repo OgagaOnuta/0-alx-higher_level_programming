bf718e7abb058148961bdf68c546bf3b9e19b1ce
David Onuta <ogagaonuta@gmail.com>
2022-12-21 03:11:09 +0000

    Write a class that defines a square and print the square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+Creates a class Square that defines a square
+'''
+
+
+class Square:
+    '''
+    Defines a square
+    '''
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if (type(self.__size) is not int):
+            raise TypeError("size must be an integer")
+        if (self.__size < 0):
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if (self.__size == 0):
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
