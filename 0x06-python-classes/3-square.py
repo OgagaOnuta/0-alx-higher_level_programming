b156c3d0fc098d4d0ede85f9d7a63ec680000416
David Onuta <ogagaonuta@gmail.com>
2022-12-21 02:28:20 +0000

    Writes a class that defines a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+Creates a class Square that defines a square
+'''
+
+
+class Square:
+    '''
+    Defines a square
+    '''
+    def __init__(self, size=0):
+        self.__size = size
+        if (type(self.__size) is not int):
+            raise TypeError("size must be an integer")
+        if (self.__size < 0):
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return (self.__size * self.__size)
