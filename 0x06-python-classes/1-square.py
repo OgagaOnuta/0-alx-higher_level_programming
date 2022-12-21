f02debe7fb5525437b1c5f2e4a53002c666a274d
David Onuta <ogagaonuta@gmail.com>
2022-12-21 01:57:06 +0000

    Write a class that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+Creates a class Square that defines a square
+'''
+
+
+class Square:
+    '''
+    Defines a square
+    '''
+    def __init__(self, size):
+        self.__size = size
