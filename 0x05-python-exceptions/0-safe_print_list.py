dda54a25a460beac9afbe81824b97bd76a455d5e
David Onuta <ogagaonuta@gmail.com>
2022-12-22 02:15:59 +0000

    Print elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    num = int(0)
+
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            num += 1
+        print()
+    except IndexError:
+        print()
+        
+    return (num)
