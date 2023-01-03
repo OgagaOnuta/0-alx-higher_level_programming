0b985982d46d337f5e4ea20e52ae0095b40cf04e
David Onuta <ogagaonuta@gmail.com>
2023-01-03 02:22:45 +0000

    Create the repr method of the class

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Creates a class that defines a rectangle
+"""
+
+
+class Rectangle:
+    """Defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+        if (type(self.__width) is not int):
+            raise TypeError("width must be an integer")
+        if (self.__width < 0):
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+        if (type(self.__height) is not int):
+            raise TypeError("height must be an integer")
+        if (self.__height < 0):
+            raise ValueError("height must be >= 0")
+
+    def area(self):
+        """Returns the area of a rectangle"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Returns the perimeter of a rectangle"""
+        if (self.__width == 0 or self.__height == 0):
+            return (0)
+        return (2 * (self.__width + self.__height))
+
+    def __str__(self):
+        if (self.__width == 0 or self.__height == 0):
+            return ("")
+        return (("#" * self.__width) +
+                ("\n" + "#" * self.__width) * (self.__height - 1))
+
+    def __repr__(self):
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
