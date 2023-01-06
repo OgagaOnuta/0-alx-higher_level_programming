36a3abd6c35f2ea3c9cc8109602293cf076896f1
David Onuta <ogagaonuta@gmail.com>
2023-01-06 12:58:15 +0000

    Defines a square and prints it

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -45,6 +45,8 @@ class Square:
         if (self.__size == 0):
             print()
         else:
+            for i in range(self.__position[1]):
+                print("\n" * self.__position[1], end="")
             for i in range(self.__size):
                 print(" " * self.__position[0], end="")
                 print("#" * self.__size)
