9335422cc25c2a84748fdc1045e6fd5b3c4e9d27
David Onuta <ogagaonuta@gmail.com>
2023-01-06 16:49:31 +0000

    Add 2 integers and test the function

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Adds 2 integers
+"""
+
+def add_integer(a, b=98):
+    """Adds a + b
+    
+    Args:
+        a (int): first integer
+        b (int): second integer
+
+    Returns:
+        a + b
+    """
+    if ((type(a) is not int) and (type(a) is not float)):
+        raise TypeError("a must be an integer")
+    if ((type(b) is not int) and (type(b) is not float)):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
