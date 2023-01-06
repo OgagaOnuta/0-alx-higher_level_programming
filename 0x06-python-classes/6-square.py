185167cb2b02f86d5ad63f2c4417ef15289e1a72
David Onuta <ogagaonuta@gmail.com>
2023-01-06 12:58:15 +0000

    Defines a square and prints it

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -32,7 +32,9 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif (len(value) != 2):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif ((type(value[0]) is not int) and (type(value[1]) is not int)):
+        elif ((type(value[0]) is not int) or (type(value[1]) is not int)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif ((value[0] < 0) or (value[1] < 0)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
