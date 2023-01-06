344068a073ba129b1d51032527a7b4339cf555ae
David Onuta <ogagaonuta@gmail.com>
2023-01-06 12:58:15 +0000

    Defines a square and prints it

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -28,9 +28,8 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (type(value) is not tuple):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif (len(value) != 2):
+        if ((type(value) is not tuple) and (len(value) != 2)
+                and (value[0] is not int) and (value[1] is not int)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
