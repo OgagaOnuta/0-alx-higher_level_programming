a3194a672342f3927a6cf7011585bfc77a7d7e33
David Onuta <ogagaonuta@gmail.com>
2023-01-10 23:53:18 +0000

    Read a text file and print to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,11 @@ and prints it to standard output
 
 
 def read_file(filename=""):
-    """ Reads a text file and prints it to stdout """
+    """
+    Reads a text file and prints it to stdout
+
+    Args:
+        filename : name of file
+    """
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read())
