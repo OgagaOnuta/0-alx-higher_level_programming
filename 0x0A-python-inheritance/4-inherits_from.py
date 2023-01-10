223e6400b4c673db8dacade31d1e88a108670e21
David Onuta <ogagaonuta@gmail.com>
2023-01-10 03:00:18 +0000

    Check if object is a subclass of class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Defines a function that checks if an 
+object is a subclass of a specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Check if object is a subclass of a class
+
+    Args:
+        obj (object)
+        a_class (class)
+
+    Return:
+        True if instance
+        False if not instance
+    """
+    if (issubclass(obj, a_class)):
+        return (True)
+    else:
+        return (False)
