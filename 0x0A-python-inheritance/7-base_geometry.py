eed4fa44c7ddb899f40b89035ec2389b82349ef6
David Onuta <ogagaonuta@gmail.com>
2023-01-11 16:40:17 +0000

    Create a Base Geometry class

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Creates an class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ A Base Geometry class """
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if (type(value) is not int):
+            raise TypeError("{} must be an integer".format(name))
+        if (value <= 0):
+            raise ValueError("{} must be greater than 0".format(name))
