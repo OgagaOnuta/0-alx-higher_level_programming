102745bd365f3b80052f9efb8e496c1aa610ac95
David Onuta <ogagaonuta@gmail.com>
2023-01-11 16:40:17 +0000

    Create a Base Geometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Creates an class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ A Base Geometry class """
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
