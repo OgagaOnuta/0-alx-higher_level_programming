324f3e034a9b11219e40373fe7b65e6ce928eb20
David Onuta <ogagaonuta@gmail.com>
2023-01-11 00:21:19 +0000

    Return the JSON representation of an object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Creates a function that returns the
+JSON representation of an object (string)
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """ Returns the JSON representation of an object """
+    return (json.dumps(my_obj))
