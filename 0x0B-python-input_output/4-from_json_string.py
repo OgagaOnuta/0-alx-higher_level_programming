9a7aadc82ed3e0e518b5dd430f7036f5d8fd838a
David Onuta <ogagaonuta@gmail.com>
2023-01-11 00:32:44 +0000

    Return an object represented by a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Creates a function that returns an object
+represented by a JSON string
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns an object represented by a JSON string
+
+    Args:
+        my_str (string)
+    """
+    return (json.loads(my_str))
