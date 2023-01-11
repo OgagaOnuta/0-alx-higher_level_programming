2f386305f5cf45154f5c823d8cce7156ad08c721
David Onuta <ogagaonuta@gmail.com>
2023-01-11 18:11:23 +0000

    Create class Square that inherits from Rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,7 +9,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     def __init__(self, size):
-        Rectangle.__init__(self, size, size)
         self.integer_validator("size", size)
         self.__size = size
+        Rectangle.__init__(self, self.__size, self.__size)
         super().area()
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -9,9 +9,9 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     def __init__(self, size):
-        Rectangle.__init__(self, size, size)
         self.integer_validator("size", size)
         self.__size = size
+        Rectangle.__init__(self, self.__size, self.__size)
         super().area()
 
     def __str__(self):
