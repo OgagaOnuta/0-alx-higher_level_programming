34dbbc46bb8c5a7adcdca6a550038f4d8b304cda
David Onuta <ogagaonuta@gmail.com>
2023-02-15 03:56:54 +0000

    List all records of a table of a database in MySQL server

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,7 +1,5 @@
 -- Lists all records of a table of a database in MySQL server
 SELECT score, name
 FROM second_table
-ORDER BY score DESC
-WHERE name =
-      (SELECT name
-      FROM second_table);
+WHERE (name != '')
+ORDER BY score DESC;
