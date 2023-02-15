1c8183f2762f6081e28adebc523414d03029a814
David Onuta <ogagaonuta@gmail.com>
2023-02-15 03:02:06 +0000

    Create a table in a database in MySQL server and add multiple rows

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,15 @@
+-- Creates a table in a database in MySQL server and add multiple rows
+
+-- Create the table
+CREATE TABLE IF NOT EXISTS second_table
+(id INT, 
+name VARCHAR(256), 
+score INT);
+
+-- Insert rows
+INSERT INTO second_table(id, name, score)
+VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
