730c37db7a1f09aabf9ad42336bea54de5372f4a
David Onuta <ogagaonuta@gmail.com>
2023-02-22 13:30:33 +0000

    List details using a subquery in MySQL

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,7 @@
 -- Lists all detail found in a database that matches a certain condition
 SELECT id, name
 FROM cities
-WHERE (SELECT id FROM states
+WHERE (SELECT id
+      FROM states
       WHERE (name = 'California'))
 ORDER BY cities.id;
