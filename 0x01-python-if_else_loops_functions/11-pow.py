fd847508201d41f0ba01214316a0bee3001c26b0
David Onuta <ogagaonuta@gmail.com>
2023-03-12 00:26:40 +0000

    Raise an integer to the power of another integer

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Raises an integer to the power of another integer'''
+
+def pow(a, b):
+    '''Computes a to the power of b
+    
+       Args:
+           a (int): The first parameter
+           b (int): The second parameter
+
+       Returns:
+           a ** b
+    '''
+    return (a ** b)
