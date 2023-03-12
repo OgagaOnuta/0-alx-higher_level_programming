3746e5e0ccd8bfe82d6a033e23bbe6ae1a7e17b1
David Onuta <ogagaonuta@gmail.com>
2023-03-12 00:20:20 +0000

    Add two integers

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-'''Adds two integers'''
 
 def add(a, b):
-    '''Args:
-           a (int): The first parameter
-           b (int): The second parameter
+    '''Adds two integers
 
-       Returns:
-           a + b
+    Args:
+        a (int): The first parameter
+        b (int): The second parameter
+
+    Returns:
+        a + b
     '''
     return (a + b)
