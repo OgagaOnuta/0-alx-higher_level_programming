9575ca03c7aeaa7a00cfd6c2b041df69a425b1f4
David Onuta <ogagaonuta@gmail.com>
2023-03-13 23:53:44 +0000

    Print square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,22 +7,16 @@ if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
   const x = parseInt(process.argv[2]);
-
-  const square = function squareChar () {
-    let str = '';
-    let i = 0;
-
-    while (i < x) {
-      str += 'X';
-      i++;
-    }
-
-    return (str);
-  };
-
+  let str = '';
   let i = 0;
+  let j = 0;
+
   while (i < x) {
-    console.log(square());
+    while (j < x) {
+      str += 'X';
+      j++;
+    }
+    console.log(str);
     i++;
   }
 }
