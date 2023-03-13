733089aa033ae14973549a0a451bda059d9294e1
David Onuta <ogagaonuta@gmail.com>
2023-03-13 23:53:44 +0000

    Print square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+// Prints a square
+
+const process = require('process');
+
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  const x = parseInt(process.argv[2]);
+
+  const str = () => {
+    let str = '';
+    let i = 0;
+
+    while (i < x) {
+      str += 'X';
+      i++;
+    }
+    return (str);
+  }
+
+  let i = 0;
+  while (i < x) {
+    console.log(str());
+    i++;
+  }
+}
