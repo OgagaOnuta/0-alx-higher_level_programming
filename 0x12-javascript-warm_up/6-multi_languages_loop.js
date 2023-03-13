acdf879f81a041de1fc27d243535834cbf0fe0e7
David Onuta <ogagaonuta@gmail.com>
2023-03-13 23:01:37 +0000

    Print strings using an array and a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Print strings using an array and a loop
+
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+
+while (i < myArray.length) {
+  console.log(myArray[i]);
+  i++;
+}
