b49156c4dc227c8aebdc040e6e1baa7f932143d4
David Onuta <ogagaonuta@gmail.com>
2023-03-14 02:21:04 +0000

    Execute a function a certain number of times

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Executes a function a certain number of times
 
-module.exports.callMeMoby = function (x, theFunction) {
-  while (x) {
+exports.callMeMoby = function (x, theFunction) {
+  while (x > 0) {
     theFunction();
     x--;
   }
