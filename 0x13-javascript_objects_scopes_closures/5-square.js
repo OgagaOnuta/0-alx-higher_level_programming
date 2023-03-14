d6c9acfcdfe8c3b7338eaa21a2453082c9438951
David Onuta <ogagaonuta@gmail.com>
2023-03-14 20:35:23 +0000

    Create a class that inherits from another class

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Creates a class that inherits from another class
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+};
