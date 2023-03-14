8edb92f862e21b9d51d9bb2f274659d742369ea6
David Onuta <ogagaonuta@gmail.com>
2023-03-14 01:25:19 +0000

    Search for the second biggest integer in the list of arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,21 +5,8 @@ const process = require('process');
 
 const len = process.argv.length;
 
-// Function to create an array from the list of arguments
-function createArray () {
-  const myArray = [];
-  let i = 2;
-
-  while (i < len) {
-    myArray.push(parseInt(process.argv[i]));
-    i++;
-  }
-
-  return (myArray);
-}
-
 // Function to search for the biggest element in the array
-function searchBiggest (givenArray) {
+const bigger = function searchBiggest (givenArray) {
   const givenLength = givenArray.length;
   let i = 2;
   let bigger;
@@ -38,15 +25,28 @@ function searchBiggest (givenArray) {
   }
 
   return (bigger);
-}
+};
 
 if ((len === 2) || (len === 3)) {
   console.log(0);
 } else if (len > 3) {
-  const myArray = createArray(); // Create the array
-  const big = searchBiggest(myArray); // Search for the biggest element
-  const myIndex = myArray.indexOf(big); // Get the index of the biggest element
-  myArray.splice(myIndex, 1); // Remove the biggest element from the array
-  const secondBiggest = searchBiggest(myArray); // Search for the current biggest element
+  // Create the array
+  const myArray = function createArray () {
+    const myArray = [];
+    let i = 2;
+
+    while (i < len) {
+      myArray.push(parseInt(process.argv[i]));
+      i++;
+    }
+
+    return (myArray);
+  };
+
+  const newArray = myArray();
+  const big = bigger(newArray); // Search for the biggest element
+  const myIndex = newArray.indexOf(big); // Get the index of the biggest element
+  newArray.splice(myIndex, 1); // Remove the biggest element from the array
+  const secondBiggest = bigger(newArray); // Search for the current biggest element
   console.log(secondBiggest); // Print it to the console
 }
