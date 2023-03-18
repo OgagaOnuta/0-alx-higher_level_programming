a265982a6dde00b681fc614b4857eb641ebf4335
David Onuta <ogagaonuta@gmail.com>
2023-03-18 02:29:11 +0000

    Take in an argument and list filtered data that matches the argument

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Takes in an argument and lists filtered data from
+a table in a database that matches the argument'''
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+    WHERE (states.name = '{}') \
+    ORDER BY states.id".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
