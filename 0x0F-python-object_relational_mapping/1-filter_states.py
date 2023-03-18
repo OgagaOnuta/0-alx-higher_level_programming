acf9f131f4d2429304953ab0a22707d76377ef4c
David Onuta <ogagaonuta@gmail.com>
2023-03-18 02:01:53 +0000

    List filtered data from a table in a database

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Lists filtered data from a table in a database'''
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+    WHERE states.name \
+    LIKE BINARY 'N%' \
+    ORDER BY states.id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
