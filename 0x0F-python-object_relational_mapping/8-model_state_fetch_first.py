df453fbcc7498be9b974b525b4aef2ddc67b3dca
David Onuta <ogagaonuta@gmail.com>
2023-03-19 02:15:55 +0000

    List all data from a table in a database using SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Lists all data from a table in a database'''
+
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    state = session.query(State).order_by(State.id).first()
+    if (state is None):
+        print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
