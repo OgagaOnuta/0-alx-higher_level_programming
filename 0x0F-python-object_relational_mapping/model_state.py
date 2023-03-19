0cf91df8e244162c649fb13dcff11e8924050cb9
David Onuta <ogagaonuta@gmail.com>
2023-03-19 00:44:26 +0000

    Add files for class definition and execution using SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Creates class definition of State'''
+
+from sqlalchemy import (Column,
+                        Integer,
+                        String)
+
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    '''Class definition of State'''
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, unique=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
