7aa0beb9c56a5ace5181b9d68e0c4d8b07de5857
David Onuta <ogagaonuta@gmail.com>
2023-03-19 17:06:45 +0000

    Update row of table in a database using SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Lists all data from a table in a database'''
+
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    session.query(State).filter(State.id == 2).update(
+        {State.name: 'New Mexico'},
+        synchronize_session='evaluate')
+
+    session.commit()
