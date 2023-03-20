93bac95a4f4e61c70bbc7f3bb06207e7005998a8
David Onuta <ogagaonuta@gmail.com>
2023-03-20 01:25:45 +0000

    Add rows to related tables in a database using SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Add data to related tables in a database'''
+
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    state = State(name='California')
+    city = City(name='San Francisco')
+    state.cities.append(city)
+
+    session.add(state)
+    session.commit()
