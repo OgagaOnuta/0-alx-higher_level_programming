a596ff9be9473643f92f89e639f70ce0be6a906e
David Onuta <ogagaonuta@gmail.com>
2023-03-20 02:16:03 +0000

    List all data in related tables in a database

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Lists all data in related tables in a database'''
+
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    for state in \
+        session.query(State)\
+               .join(City)\
+               .order_by(State.id, City.id).all():
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
